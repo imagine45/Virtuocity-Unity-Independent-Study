bc62aad1d948c28a04dd8cbcc5515e7fcb1ba2db
Lucas Holden <lucasbholden@gmail.com>
2024-05-07 20:43:37 -0700

    whoops (hotfix)

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -37,7 +37,7 @@ public class MenuButton : MonoBehaviour
 
     public void continueGame()
     {
-        SettingsManager.instance.loadedFromContinue = true;
+        SettingsManagement.instance.loadedFromContinue = true;
         SceneManager.LoadScene(SceneManager.GetSceneAt(SettingsManagement.instance.currentScene).name);
     }
 
